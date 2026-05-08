9e7c2912d259944fae4939abbd258ee36ac7c0e6
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-05-07 21:36:09 -0700

    Fix copied budget_estimation.py content

--- budget_estimation.py
+++ budget_estimation.py
@@ -52,16 +52,15 @@ def budget_calc(org, dest, days, date:list , people_number=None, local_constrain
 
 
     elif grain == "state":
-        city_set = open('../database/background/citySet_with_states.txt').read().strip().split('
-')
+        city_set = open('../database/background/citySet_with_states.txt').read().strip().split('\n')
         
         all_hotel_data = []
         all_restaurant_data = []
         all_flight_data = []
         
         for city in city_set:
-            if dest == city.split('	')[1]:
-                candidate_city = city.split('	')[0]
+            if dest == city.split('\t')[1]:
+                candidate_city = city.split('\t')[0]
                 
                 # Fetch data for the current city
                 current_hotel_data = hotel.run(candidate_city)
@@ -176,3 +175,4 @@ def budget_calc(org, dest, days, date:list , people_number=None, local_constrain
         budgets[mode] = total_budget
 
     return budgets
+
