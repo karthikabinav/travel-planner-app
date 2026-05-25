98b7a8ca5577d381d68d2bcf159bcb15c5c743e3
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-05-24 23:28:17 -0700

    Add budget estimation utility

--- budget_estimation.py
+++ budget_estimation.py
@@ -24,7 +24,6 @@ def estimate_budget(data, mode):
         data = [x for x in data if str(x) != 'nan']
         return sum(data) / len(data)
     
-
 def budget_calc(org, dest, days, date:list , people_number=None, local_constraint = None):
     """
     Calculate the estimated budget for all three modes: lowest, highest, average.
@@ -176,3 +175,4 @@ def budget_calc(org, dest, days, date:list , people_number=None, local_constrain
         budgets[mode] = total_budget
 
     return budgets
+
