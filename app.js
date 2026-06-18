ea22b53e74fd89084810b33736b2a75e6874e668
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-06-18 06:34:35 -0700

    Add basic Express server setup

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Travel Planner!' );
+  res.send('Welcome to Travel Planner!');
 });
 
 app.listen(3000, () => {
